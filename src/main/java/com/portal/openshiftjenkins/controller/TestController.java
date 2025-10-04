eca18b2d58c16e2d767df0f284b0939743501d9c
Kyle Canonigo <kyleanoracanonigo@gmail.com>
2025-10-04 05:55:37 +0000

    Edit TestController.java

--- src/main/java/com/portal/openshiftjenkins/controller/TestController.java
+++ src/main/java/com/portal/openshiftjenkins/controller/TestController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+	@GetMapping("/")
+    public String root() {
+        return "App is running!";
+    }
+
 	@GetMapping("/hello")
 	public String ping() {
 		return "Openshift Pipe Line Testing";
